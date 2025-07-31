a9abd80f3364b87cd1c79380da46fa56135061c2
Kevin Costner <dk.costner@yahoo.com>
2025-07-31 14:29:29 +0400

    fix: use relative imports for Navbar/Footer in ClientNav

--- src/components/ClientNav.tsx
+++ src/components/ClientNav.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Navbar from "@/components/Navbar";   // <-- match case
-import Footer from "@/components/Footer";   // <-- match case
-
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 export default function ClientNav({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
