b6a63b5deb6b759710026ea7c4fc4d30db842083
Kevin Costner <dk.costner@yahoo.com>
2025-08-05 14:31:14 +0400

    fix: remove unused @ts-expect-error and tidy rater/user layouts

--- src/app/user/layout.tsx
+++ src/app/user/layout.tsx
@@ -1,20 +1,26 @@
 // src/app/user/layout.tsx
-import React from 'react'
-import UserNavBar from './navbar'
-import UserFooter from './footer'
+import type { Metadata } from "next";
+import UserNavBar from "./navbar";
+import UserFooter from "./footer";
 
-export default function UserLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: "Percepia • User",
+};
+
+export default function UserLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en">
-      <body>
-        <div className="min-h-screen flex flex-col">
-          {/* User-only navbar */}
-          {/* @ts-expect-error Server Component renders Client Component */}
-          <UserNavBar />
-          <main className="flex-1">{children}</main>
-          <UserFooter />
-        </div>
-      </body>
-    </html>
-  )
+    <div className="min-h-screen flex flex-col">
+      {/* User-only navbar */}
+      <UserNavBar />
+
+      <main className="flex-1">{children}</main>
+
+      {/* User-only footer */}
+      <UserFooter />
+    </div>
+  );
 }
